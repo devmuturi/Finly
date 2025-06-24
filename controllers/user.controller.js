846e64e2378186b550420d5a9ded6d0a858fe750
devmuturi <muturinjonge09@gmail.com>
2025-06-24 12:20:35 +0300

    Fix mongodb users collection

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,22 @@
-const User = require('../libs/models/user.model');
+const User = require('../libs/models/user.model')
 
 const createUser = async (req, res) => {
   await User.create({
-    email: 'frednjonge9@gmail.com',
-    password: 'password'
+    email: 'frednjonge90@gmail.com',
+    password: 'password',
   })
 
-  res.render('user', { message: 'User Created', user: null });
+  res.render('user', { message: 'User Created', user: null })
 }
 
 const getUser = async (req, res) => {
-  const user = await User.findOne({ email: 'frednjonge9@gmail.com' })
+  const user = await User.findOne({ email: 'frednjonge90@gmail.com' })
 
-  res.render('user', { message: 'User Retrieved', user: user})
+  res.render('user', { message: 'User Retrieved', user: user })
 }
 
 const deleteUser = async (req, res) => {
-  await User.findOneAndDelete({ email: 'fredgjonge9@gmail.com' })
+  await User.findOneAndDelete({ email: 'frednjonge90@gmail.com' })
 
   res.render('user', { message: 'User Deleted', user: null })
 }
