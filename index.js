088fdc1b7fb60bd19355e7bc61c417753dfebd18
devmuturi <muturinjonge09@gmail.com>
2025-06-25 13:23:06 +0300

    Cleaning the routes

--- index.js
+++ index.js
@@ -28,19 +28,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 // routes
-app.get('/', (req, res) => {
-  res.render('index', { message: 'Hello from Node.js' })
-})
 
-app.use('/users', userRouter)
-
-app.get('/contact', (req, res) => {
-  res.render('index', { message: 'The Contact Page' })
-})
-
-app.get('/about', (req, res) => {
-  res.render('index', { message: 'The About Page' })
-})
+app.use('/', userRouter)
 
 app.get('/*splat', (req, res) => {
   res.status(404).render('index', { message: 'Not Found' })
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,14 +1,24 @@
 const { Router } = require('express')
 const router = Router()
 
-const {
-  getUser,
-  createUser,
-  deleteUser,
-} = require('../controllers/user.controller')
+// const {
+//   getUser,
+//   createUser,
+//   deleteUser,
+// } = require('../controllers/user.controller')
 
-router.get('/', getUser)
-router.get('/create', createUser)
-router.get('/delete', deleteUser)
+router.get('/', (req, res) => {
+  res.render('pages/index', { title: 'Finly' })
+})
+router.get('/login', (req, res) => {
+  res.render('pages/login', {
+    title: 'Sign in',
+  })
+})
+router.get('/signup', (req, res) => {
+  res.render('pages/signup', {
+    title: 'Sign up',
+  })
+})
 
 module.exports = router
