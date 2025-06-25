c1d4a57b99cd3b8860ab34be4809bd6dcab6e2a7
devmuturi <muturinjonge09@gmail.com>
2025-06-25 13:15:30 +0300

    Implementing express session

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const path = require('node:path')
 const userRouter = require('./routes/user.route')
+const session = require('express-session')
 
 require('dotenv').config()
 require('./libs/dbConnect')
@@ -9,13 +10,22 @@ require('./libs/dbConnect')
 const app = express()
 
 // Add css
+app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(
+  session({
+    // key used to sign in session and verify the cookie later
+    secret: process.env.AUTH_SECRET,
+    // both used to optimize the session storage
+    saveUninitialized: true,
+    resave: false,
+  }),
+)
 
 // app.set('views', './views');
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-app.use(morgan('dev'))
 
 // routes
 app.get('/', (req, res) => {
