1629b9af795a8cd3fb61020710bfafa8f2fc3429
devmuturi <muturinjonge09@gmail.com>
2025-06-26 15:16:42 +0300

    Updating user routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,17 @@
-const { validateSignUp, signup } = require('../controllers/user.controller')
+const {
+  validateSignUp,
+  signup,
+  validateLogin,
+  login,
+  logout,
+} = require('../controllers/user.controller')
 const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.render('pages/index', { title: 'Finly' })
-})
-router.get('/login', (req, res) => {
-  res.render('pages/login', {
-    title: 'Sign in',
+  res.render('pages/index', {
+    title: 'Finly',
+    info: req.flash('info')[0],
   })
 })
 
@@ -19,6 +23,19 @@ router.get('/signup', (req, res) => {
     errors: req.flash('errors'),
   })
 })
+
+router.get('/login', (req, res) => {
+  res.render('pages/login', {
+    title: 'Sign in',
+    user: req.flash('data')[0],
+    info: req.flash('info')[0],
+    errors: req.flash('errors'),
+  })
+})
+
+router.post('/login', validateLogin, login)
+
+router.get('/logout', logout)
 
 router.post('/signup', validateSignUp, signup)
 
