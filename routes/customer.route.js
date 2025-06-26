ed59b8a77f0a08b2a18cd82aad339f417d7cc1f5
devmuturi <muturinjonge09@gmail.com>
2025-06-27 00:50:53 +0300

    Add create routes

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-const { showCustomers } = require('../controllers/customer.controller')
+const {
+  showCustomers,
+  createCustomer,
+  validateCustomer,
+} = require('../controllers/customer.controller')
 
 router.get('/', showCustomers)
+
+router.get('/create', function (req, res) {
+  res.render('pages/customers', {
+    title: 'Create Customer',
+    formAction: 'create',
+    type: 'form',
+    customer: req.flash('data')[0],
+    errors: req.flash('errors'),
+  })
+})
+
+router.post('/create', validateCustomer, createCustomer)
 
 module.exports = router
