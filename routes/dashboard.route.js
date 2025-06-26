fc3c94587b46b1974b9bbb0acfd29cc91be1088d
devmuturi <muturinjonge09@gmail.com>
2025-06-26 17:45:29 +0300

    Creating customer routes

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -1,11 +1,15 @@
 const { Router } = require('express')
 const router = Router()
 
+const customersRouter = require('./customer.route')
+
 router.get('/', (req, res) => {
   res.render('pages/dashboard', {
     title: 'Dashboard',
     info: req.flash('info')[0],
   })
 })
+
+router.use('/customers', customersRouter)
 
 module.exports = router
